19997ea338c7e662c20af81cc8044b2d87161e88
Yan Yan Ji <Jankaji@inmail.sk>
2020-09-10 23:06:31 +0200

    all

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class Calculator {
+
+    public static double sumNumbers(double a, double b) {
+        return a + b;
+    }
+
+    public static double subNumbers (double a, double b){
+        return a - b;
+    }
+
+    public static double multiNumbers (double a, double b){
+        return a * b;
+    }
+
+    public static double divideNumbers (double a, double b) {
+        return a / b;
+    }
+
+    public static double percentage (double a) {
+        return a / 100;
+    }
+
+
+}
---
+++ Controller.java
@@ -0,0 +1,89 @@
+package sample;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML // fx: id=textDisplay
+    private Text textDisplay;
+    private String prev = "";
+    private double num;
+    private String operation = "None";
+    private boolean check = false;
+
+    private static final String ERROR = "ERROR";
+
+    public void buttonPressedNumber(Event event) {
+        Button button = (Button) event.getSource();
+        if (check) {
+            num = Double.parseDouble(prev);
+            prev = "";
+            check = false;
+        }
+
+        if (prev.length() < 6) {
+            this.textDisplay.setText(prev + button.getText());
+            prev = prev + button.getText();
+        }
+    }
+
+    public void buttonPressedFloat (Event event){
+        Button button = (Button) event.getSource();
+        if (prev.length() < 5 && !prev.contains(".") && prev.length() > 0) {
+            this.textDisplay.setText(prev + button.getText());
+            prev = prev + button.getText();
+        }
+    }
+
+    public void buttonPressedOperation(Event event) {
+        Button button = (Button) event.getSource();
+        operation = button.getText();
+        System.out.println(operation);
+        check = true;
+    }
+
+    public void buttonPressedEqual(Event event) {
+        double result = 0;
+        double num2 = Double.parseDouble(prev);
+        String text;
+
+        switch (operation) {
+            case "+":
+                result = Calculator.sumNumbers(num, num2);
+                text = String.format("%4.2f%n",result);
+                this.textDisplay.setText(text);
+                break;
+            case "-":
+                result = Calculator.subNumbers(num, num2);
+                text = String.format("%4.2f%n",result);
+                this.textDisplay.setText(text);
+                break;
+            case "x":
+                result = Calculator.multiNumbers(num, num2);
+                text = String.format("%4.2f%n",result);
+                this.textDisplay.setText(text);
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = Calculator.divideNumbers(num, num2);
+                    text = String.format("%4.2f%n",result);
+                    this.textDisplay.setText(text);
+                } else {
+                    this.textDisplay.setText(ERROR);
+                }
+                break;
+        }
+        prev = "";
+        num = 0;
+    }
+
+    public void buttonPressedPercentage(Event event) {
+        double num = Double.parseDouble(prev);
+        num = Calculator.percentage(num);
+        prev = Double.toString(num);
+
+    }
+}
