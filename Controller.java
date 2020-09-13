be4e37d7268e55c84b646bb13710d10904f60806
Yan Yan Ji <Jankaji@inmail.sk>
2020-09-13 15:06:06 +0200

    handle too big number

--- Controller.java
+++ Controller.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class Controller {
-    //TODO: Fix scientific notation to general form
+    //TODO: Showing scientific notation
     //      Error handling
 
     @FXML // fx: id=textDisplay
@@ -15,20 +15,12 @@ public class Controller {
     private double number;
     private String operation = "None";
 
-    private static final String ERROR = "ERROR";
+    private static final String ERROR = "REPETITION ERROR";
 
     public void buttonPressedNumber(Event event) {
         Button button = (Button) event.getSource();
 
         if (numMemory.length() < 7) {
-            numMemory += button.getText();
-            this.textDisplay.setText(numMemory);
-        }
-    }
-
-    public void buttonPressedFloat(Event event) {
-        Button button = (Button) event.getSource();
-        if (numMemory.indexOf(".") == -1) {
             numMemory += button.getText();
             this.textDisplay.setText(numMemory);
         }
@@ -43,6 +35,19 @@ public class Controller {
         numMemory = "";
     }
 
+    public void buttonPressedFloat(Event event) {
+        Button button = (Button) event.getSource();
+        if (numMemory.indexOf(".") == -1) {
+            numMemory += button.getText();
+            this.textDisplay.setText(numMemory);
+        }
+    }
+
+    public void buttonPressedPercentage(Event event){
+        double number = Double.parseDouble(numMemory);
+        number = Calculator.percentage(number);
+        numMemory = Double.toString(number);
+    }
     public void buttonPressedEqual(Event event) {
         switch (operation) {
             case "+":
@@ -69,102 +74,26 @@ public class Controller {
         numMemory = "";
     }
 
-    public void buttonRestart(Event event){
+    public void buttonPressedRestart(Event event){
         numMemory = "";
         number = 0;
         operation = "None";
     }
 
     private String formatTextNumber(double number) {
-        String text = Double.toString(number);
+        String text;
+        if (number > 999999){
+            text = "NaN";
+        } else {
+            text = Double.toString(number);
+        }
 
         if (text.lastIndexOf(".0") > text.length() - 3) {
             return text.substring(0, text.indexOf("."));
         } else if (text.length() > 7) {
-            return text.substring(0, 6);
+            return text.substring(0, 7);
         } else {
             return text;
         }
     }
-
-    /* @FXML // fx: id=textDisplay
-     private Text textDisplay;
-     private String prev = "";
-     private double num;
-     private String operation = "None";
-     private boolean check = false;
-
-     private static final String ERROR = "ERROR";*/
-
-/*
-    public void buttonPressedNumber(Event event) {
-        Button button = (Button) event.getSource();
-        if (check) {
-            num = Double.parseDouble(prev);
-            prev = "";
-            check = false;
-        }
-
-        if (prev.length() < 6) {
-            this.textDisplay.setText(prev + button.getText());
-            prev = prev + button.getText();
-        }
-    }
-
-    public void buttonPressedFloat (Event event){
-        Button button = (Button) event.getSource();
-        if (prev.length() < 5 && !prev.contains(".") && prev.length() > 0) {
-            this.textDisplay.setText(prev + button.getText());
-            prev = prev + button.getText();
-        }
-    }
-
-    public void buttonPressedOperation(Event event) {
-        Button button = (Button) event.getSource();
-        operation = button.getText();
-        System.out.println(operation);
-        check = true;
-    }
-
-    public void buttonPressedEqual(Event event) {
-        double result = 0;
-        double num2 = Double.parseDouble(prev);
-        String text;
-
-        switch (operation) {
-            case "+":
-                result = Calculator.sumNumbers(num, num2);
-                text = String.format("%4.2f%n",result);
-                this.textDisplay.setText(text);
-                break;
-            case "-":
-                result = Calculator.subNumbers(num, num2);
-                text = String.format("%4.2f%n",result);
-                this.textDisplay.setText(text);
-                break;
-            case "x":
-                result = Calculator.multiNumbers(num, num2);
-                text = String.format("%4.2f%n",result);
-                this.textDisplay.setText(text);
-                break;
-            case "/":
-                if (num2 != 0) {
-                    result = Calculator.divideNumbers(num, num2);
-                    text = String.format("%4.2f%n",result);
-                    this.textDisplay.setText(text);
-                } else {
-                    this.textDisplay.setText(ERROR);
-                }
-                break;
-        }
-        prev = "";
-        num = 0;
-    }
-
-    public void buttonPressedPercentage(Event event) {
-        double num = Double.parseDouble(prev);
-        num = Calculator.percentage(num);
-        prev = Double.toString(num);
-
-    }*/
 }
